f43dca288f633d11737efd52e2076339e7261bdd
Walter Qian <walterqian16@gmail.com>
2024-03-18 17:23:42 +0100

    fix: csv parser

--- wallet_querier/csv_parser.py
+++ wallet_querier/csv_parser.py
@@ -2,16 +2,15 @@ import csv
 
 
 def parse_csv():
-    FILE_NAME = "./ETH_1H.csv"
+    FILE_NAME = "./ETH_USD.csv"
 
     res = []
     with open(FILE_NAME) as file:
         reader = csv.DictReader(file)
         for row in reader:
             res.append({
-                "timestamp": row["Unix Timestamp"],
                 "date": row["Date"],
-                "price": row["Open"],
+                "price": row["Close"],
             })
 
     return res
